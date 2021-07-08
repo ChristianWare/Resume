b4d8816b88083ea89c43a29acc251135ef7a2c68
Chris <chris.ware.dev@gmail.com>
2021-07-07 18:20:55 -0700

    Initial Elements

---
+++ src/components/Profile/Profile.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { Typography } from "@material-ui/core";
+import myImage from "../../assets/images/chris.jpeg";
+import CustomTimeline, { CustomTimelineSeparator } from "../Timeline/Timeline";
+import PersonOutlineOutlinedIcon from "@material-ui/icons/PersonOutlineOutlined";
+import resumeData from "../../utils/resumeData";
+import "./Profile.css";
+import TimelineItem from "@material-ui/lab/TimelineItem";
+import TimelineContent from "@material-ui/lab/TimelineContent";
+import CustomButton from "../Button/Button";
+import GetAppIcon from "@material-ui/icons/GetApp";
+
+const CustomTimelineItem = ({ title, text, link }) => (
+  <TimelineItem>
+    <CustomTimelineSeparator />
+    <TimelineContent className='timeline_content'>
+      {link ? (
+        <Typography className='timelineItem_text'>
+          <span>{title}</span>{" "}
+          <a href={link} target='_blank'>
+            {text}
+          </a>
+        </Typography>
+      ) : (
+        <Typography className='timelineItem_text'>
+          <span>{title}</span>
+          {text}
+        </Typography>
+      )}
+    </TimelineContent>
+  </TimelineItem>
+);
+
+const Profile = () => {
+  return (
+    <div className='profile container_shadow'>
+      <div className='profile_name'>
+        <Typography className='name'>{resumeData.name}</Typography>
+        <Typography className='title'>{resumeData.title}</Typography>
+      </div>
+
+      <figure className='profile_image'>
+        <img src={myImage} alt='' />
+      </figure>
+
+      <div className='profile_information'>
+        <CustomTimeline icon={<PersonOutlineOutlinedIcon />}>
+          <CustomTimelineItem title='Name: ' text={resumeData.name} />
+          <CustomTimelineItem title='Title: ' text={resumeData.title} />
+          <CustomTimelineItem title='Email: ' text={resumeData.email} />
+          <CustomTimelineItem title='Mobile: ' text={resumeData.mobile} />
+          <CustomTimelineItem title='Residence: ' text={resumeData.residence} />
+
+          {/* {Object.keys(resumeData.socials).map((key) => (
+            <CustomTimelineItem
+              title={key}
+              text={resumeData.socials[key].text}
+              link={resumeData.socials[key].link}
+            />
+          ))} */}
+        </CustomTimeline>
+        <div className='button_container'>
+          <CustomButton text={"Download Cv"} icon={<GetAppIcon />} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
---
+++ src/utils/resumeData.js
@@ -0,0 +1,193 @@
+import FacebookIcon from "@material-ui/icons/Facebook";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import {
+  AssignmentIndOutlined,
+  GitHub,
+  Language,
+  WebAssetOutlined,
+  WebOutlined,
+  YouTube,
+} from "@material-ui/icons";
+
+export default {
+  name: "Chris Ware",
+  title: "React Developer",
+  email: "c.p.ware@hotmail.com",
+  job: "Freelancer",
+  mobile: "(917) 769-1192",
+  residence: "Phoenix, AZ",
+  linkedIn: (
+    <a href='linkedin.com/in/christian-ware-88417553'>Go to My Profile</a>
+  ),
+
+  socials: {
+    Facebook: {
+      link: "https://www.facebook.com",
+      text: "Facebook.com/cw",
+      icon: <FacebookIcon />,
+    },
+    Twitter: {
+      link: "https://www.twitter.com",
+      text: "@MyTwitter",
+      icon: <TwitterIcon />,
+    },
+    LinkedIn: {
+      link: "https://www.linkedin.com",
+      text: "LinkedIn.com/cw",
+      icon: <LinkedInIcon />,
+    },
+    GitHub: {
+      link: "https://www.github.com",
+      text: "Github.com/cw",
+      icon: <GitHubIcon />,
+    },
+  },
+
+  about:
+    "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque unde vel placeat debitis sequi vitae natus fuga ratione porro. Ipsam voluptas et sequi atque reiciendis voluptate odio obcaecati officiis facilis. Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque unde vel placeat debitis sequi vitae natus fuga ratione porro. Ipsam voluptas et sequi atque reiciendis voluptate odio obcaecati officiis facilis.",
+
+  experiences: [
+    {
+      title: "Work 1",
+      date: "2018- present",
+      description:
+        "Expenses as material breeding insisted building to in. Continual so distrusts pronounce by unwilling listening. Thing do taste on we manor.",
+    },
+    {
+      title: "Work 2",
+      date: "2014-2018",
+      description:
+        "Expenses as material breeding insisted building to in. Continual so distrusts pronounce by unwilling listening. Thing do taste on we manor.",
+    },
+    {
+      title: "Work 3",
+      date: "2010-2014",
+      description:
+        "Expenses as material breeding insisted building to in. Continual so distrusts pronounce by unwilling listening. Thing do taste on we manor.",
+    },
+  ],
+
+  educations: [
+    {
+      title: "ASU",
+      date: "2018- present",
+      description:
+        "Expenses as material breeding insisted building to in. Continual so distrusts pronounce by unwilling listening. Thing do taste on we manor.",
+    },
+    {
+      title: "CUNY",
+      date: "2014-2018",
+      description:
+        "Expenses as material breeding insisted building to in. Continual so distrusts pronounce by unwilling listening. Thing do taste on we manor.",
+    },
+    {
+      title: "LaGuardia",
+      date: "2010-2014",
+      description:
+        "Expenses as material breeding insisted building to in. Continual so distrusts pronounce by unwilling listening. Thing do taste on we manor.",
+    },
+  ],
+
+  services: [
+    {
+      title: "Web Development",
+      description: "I will build you a custom website, from start to finish.",
+      icon: <WebOutlined />,
+    },
+    {
+      title: "Branding Identity",
+      description:
+        "We will male you a brand that is catchy and leaves a trace.",
+      icon: <AssignmentIndOutlined />,
+    },
+    {
+      title: "Illustrator",
+      description:
+        "I have been working in design and illustration for 6 years. ",
+      icon: <WebOutlined />,
+    },
+  ],
+
+  skills: [
+    {
+      title: "FRONT END",
+      description: [
+        "ReactJS",
+        "JavaScript",
+        "TypeScript",
+        "Bootstrap",
+        "Material UI",
+      ],
+    },
+    {
+      title: "BACK END",
+      description: ["NodeJS", "Java", "Phython", "Solidity"],
+    },
+    {
+      title: "DATABASES",
+      description: ["Firebase", "My SQL Server", "MySQL", "MongoDB"],
+    },
+    {
+      title: "SOURCE CONTROL",
+      description: ["Git", "GitHub", "SCRUM/Agile"],
+    },
+  ],
+
+  projects: [
+    {
+      tag: "React",
+      image:
+        "https://images.unsplash.com/photo-1557682250-33bd709cbe85?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1430&q=80",
+      title: "React Project 1",
+      caption: "A short description",
+      description: "This is my project description, please fill it in.",
+      links: [
+        { link: "https://www.google.com", icon: <YouTube /> },
+        { link: "https://www.google.com", icon: <GitHub /> },
+        { link: "https://www.google.com", icon: <Language /> },
+      ],
+    },
+
+    {
+      tag: "React",
+      image:
+        "https://images.unsplash.com/photo-1557682250-33bd709cbe85?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1430&q=80",
+      title: "React Project 2",
+      caption: "A short description",
+      description: "This is my project description, please fill it in.",
+      links: [
+        { link: "https://www.google.com", icon: <YouTube /> },
+        { link: "https://www.google.com", icon: <GitHub /> },
+        { link: "https://www.google.com", icon: <Language /> },
+      ],
+    },
+    {
+      tag: "Python",
+      image:
+        "https://images.unsplash.com/photo-1557682250-33bd709cbe85?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1430&q=80",
+      title: "Python Project 1",
+      caption: "A short description",
+      description: "This is my project description, please fill it in.",
+      links: [
+        { link: "https://www.google.com", icon: <YouTube /> },
+        { link: "https://www.google.com", icon: <GitHub /> },
+        { link: "https://www.google.com", icon: <Language /> },
+      ],
+    },
+    {
+      tag: "Java",
+      image:
+        "https://images.unsplash.com/photo-1557682250-33bd709cbe85?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1430&q=80",
+      title: "Java Project 1",
+      caption: "A short description",
+      description: "This is my project description, please fill it in.",
+      links: [
+        { link: "https://www.google.com", icon: <YouTube /> },
+        { link: "https://www.google.com", icon: <GitHub /> },
+        { link: "https://www.google.com", icon: <Language /> },
+      ],
+    },
+  ],
+};
